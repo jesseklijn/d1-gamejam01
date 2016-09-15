37db11ba14e0994d9443cc2de625fa3f16ee7c91
Kuno <Orzadus@gmail.com>
2016-09-15 16:14:55 +0200

    please.

---
+++ Gamejam1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody rb;
+    public float speed;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // FixedUpdate is called on a fixed time
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rb.position += new Vector3(0, 0, 1) * speed;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rb.position -= new Vector3(0, 0, 1) * speed;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.position -= new Vector3(1, 0, 0) * speed;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.position += new Vector3(1, 0, 0) * speed;
+        }
+    }
+}
+
+
